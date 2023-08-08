a7bbb91c2ca5820abcd022bec8ea2d96f54b1ca6
Bayashin <k21099kk@aitech.ac.jp>
2023-08-08 15:45:51 +0900

    add:get mysql conf

---
+++ src/conf/config.go
@@ -0,0 +1,20 @@
+package config
+
+import (
+	"github.com/spf13/viper"
+)
+
+var m *viper.Viper
+
+func GetMysqlConfig() *viper.Viper {
+
+	m = viper.New()
+	m.SetConfigType("yaml")
+	m.SetConfigName("mysql")
+	m.AddConfigPath("config/")
+
+	if err := m.ReadInConfig(); err != nil {
+		return nil
+	}
+	return m
+}
