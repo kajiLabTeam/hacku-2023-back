445c1ab1fa68d324f5dd1c19dcc79adc2374fd63
Bayashin <k21099kk@aitech.ac.jp>
2023-08-08 16:01:34 +0900

    add:connect mysql

---
+++ src/lib/sql_handler.go
@@ -0,0 +1,39 @@
+package lib
+
+import (
+	"fmt"
+	"time"
+
+	"gorm.io/gorm"
+
+	"github.com/kajiLabTeam/hacku-2023-back/src/conf"
+)
+
+func SqlConnect() {
+	var db *gorm.DB
+	var err error
+	c := conf.GetMysqlConfig()
+	dsn := c.GetString("mysql.user") + ":" + c.GetString("mysql.password") + "@" + c.GetString("mysql.protocol") + "/" + c.GetString("mysql.dbname") + "?charset=utf8&parseTime=true&loc=Asia%2FTokyo"
+
+	if db, err = gorm.Open(dsn); err != nil {
+		db = connect(dsn, 10)
+	}
+	fmt.Println("db connected!!")
+
+	return db
+}
+
+func connect(dialector gorm.Dialector, count uint) *gorm.DB {
+	var err error
+	var db *gorm.DB
+	if db, err = gorm.Open(dialector); err != nil {
+		if count > 1 {
+			time.Sleep(time.Second * 2)
+			count--
+			fmt.Printf("retry... count:%v\n", count)
+			connect(dialector, count)
+		}
+		panic(err.Error())
+	}
+	return (db)
+}
