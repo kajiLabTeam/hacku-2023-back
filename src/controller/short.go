ccaed9fbf19ed13b909edde4209f0ed8c19003e2
Bayashin <k21099kk@aitech.ac.jp>
2023-08-20 21:58:14 +0900

    add:ショート作成時のcontorollerの作成

---
+++ src/controller/short.go
@@ -0,0 +1,24 @@
+package controller
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/kajiLabTeam/hacku-2023-back/model"
+	"github.com/kajiLabTeam/hacku-2023-back/service"
+)
+
+func CreateShort(c *gin.Context) {
+	var req model.Short
+	a := c.Request.Header.Get("Authorization")
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	if err := service.Create_short(a, req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	// service.slide
+	c.JSON(http.StatusOK, req)
+}
