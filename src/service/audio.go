09f559f2142041bde37f601ff72b35fbc2a09b47
Bayashin <k21099kk@aitech.ac.jp>
2023-08-21 02:34:50 +0900

    rename:関数名と変数名の変更

--- src/service/audio.go
+++ src/service/audio.go
@@ -13,7 +13,7 @@ import (
 	"strings"
 )
 
-func GetBinary(s string) ([]byte, error) { //バイナリデータをもらってくる
+func getBinary(s string) ([]byte, error) { //バイナリデータをもらってくる
 	str := s
 	slice := []rune(str) //ルーンに変換しないと二バイト文字がバグる
 	str = string(slice)
@@ -48,19 +48,19 @@ func GetBinary(s string) ([]byte, error) { //バイナリデータをもらっ�
 	return buff.Bytes(), nil //バッファを返す
 }
 
-func MakeMp3File(b []byte, user int) (string, error) { //音声を生成する関数
+func makeMp3File(b []byte, user string) (string, error) { //音声を生成する関数
 	max := new(big.Int)                  //ファイル名の重複を避けるための乱数
-	max.SetInt64(int64(10000000))         //1000万通り
+	max.SetInt64(int64(10000000))        //1000万通り
 	r, err := rand.Int(rand.Reader, max) //乱数生成
 	if err != nil {
 		log.Printf("error: %v", err)
 		return "", err
 	}
-	path := fmt.Sprintf("%d_%d.mp3",user, r) //ファイル名の重複を避ける
-	file, _ := os.Create(path)                   //ファイル生成
+	file_name := fmt.Sprintf("%s_%d.mp3", user, r) //ファイル名の重複を避ける
+	file, _ := os.Create(file_name)                //ファイル生成
 	defer func() {
 		file.Close() //終わったら閉じる
 	}()
-	file.Write(b)    //ファイルにデータを書き込む
-	return path, nil //ファイルのパスを返す
+	file.Write(b)         //ファイルにデータを書き込む
+	return file_name, nil //ファイルのパスを返す
 }
