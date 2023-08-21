5f5c6b2fd7f27d0fa40352885b7906423094ccaf
Yusei Mizutani <yusei4520030401@gmail.com>
2023-08-21 17:52:13 +0900

    add: リアクションの削除、追加機能の実装

--- src/controller/reaction_controller.go
+++ src/controller/reaction_controller.go
@@ -1,1 +1,87 @@
 package controller
+
+import (
+	"net/http"
+	"strconv"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/kajiLabTeam/hacku-2023-back/integrations"
+	"github.com/kajiLabTeam/hacku-2023-back/model"
+)
+
+func PostReaction(c *gin.Context) {
+	auth := c.Request.Header.Get("Authorization")
+	tId := strings.TrimPrefix(auth, "Bearer ")
+	t, err := integrations.VerifyIDToken(tId)
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"message": err.Error()})
+		return
+	}
+
+	uid := t.UID
+	sId := c.Param("shortId")
+	var req ReactionRequest
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"message": err.Error()})
+		return
+	}
+
+	sIdInt, err := strconv.Atoi(sId)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": "Invalid shortId",
+		})
+		return
+	}
+
+	rId := model.GetReactionIDByName(req.Reaction)
+	if rId == nil {
+		c.JSON(http.StatusBadRequest, gin.H{"message": "Invalid reaction"})
+		return
+	}
+
+	model.InsertReaction(model.Reaction{UserID: uid, ShortID: sIdInt, ReactionListID: *rId})
+
+	c.JSON(http.StatusOK, gin.H{"reaction": req.Reaction})
+}
+
+func DeleteReaction(c *gin.Context) {
+	auth := c.Request.Header.Get("Authorization")
+	tId := strings.TrimPrefix(auth, "Bearer ")
+	t, err := integrations.VerifyIDToken(tId)
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"message": err.Error()})
+		return
+	}
+
+	uid := t.UID
+	sId := c.Param("shortId")
+	var req ReactionRequest
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"message": err.Error()})
+		return
+	}
+
+	sIdInt, err := strconv.Atoi(sId)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": "Invalid shortId",
+		})
+		return
+	}
+
+	rId := model.GetReactionIDByName(req.Reaction)
+	if rId == nil {
+		c.JSON(http.StatusBadRequest, gin.H{"message": "Invalid reaction"})
+		return
+	}
+
+	err = model.DeleteReaction(uid, sIdInt, *rId)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"message": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"reaction": req.Reaction})
+}
--- src/router/router.go
+++ src/router/router.go
@@ -15,7 +15,8 @@ func Init() {
 	r.GET("/api/user/post/history/get/", controller.GetPostingHistory)
 	r.GET("/api/user/browsing/history/", controller.GetBrowsingHistory)
 
-	r.POST("/api/short/post", controller.PostShort)
+	r.POST("/api/short/:shortId/reaction/add/", controller.PostReaction)
+	r.POST("/api/short/:shortId/reaction/remove/", controller.DeleteReaction)
 
 	r.Run(":8000")
 }
