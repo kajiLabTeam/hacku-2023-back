6f0e63c0212211391259ba545e7eca028430c948
Yusei Mizutani <yusei4520030401@gmail.com>
2023-08-24 15:58:19 +0900

    fix: ショートを見る際はログインしなくてもみれるよう修正

--- src/controller/short_controller.go
+++ src/controller/short_controller.go
@@ -11,16 +11,21 @@ import (
 )
 
 func PostShort(c *gin.Context) {
-	auth := c.Request.Header.Get("Authorization")
-	tId := strings.TrimPrefix(auth, "Bearer ")
-	t, err := integrations.VerifyIDToken(tId)
-	if err != nil {
-		c.JSON(http.StatusUnauthorized, gin.H{"error": err.Error()})
-		return
-	}
-
-	uid := t.UID
+	var uid string
 	var req model.ShortPost
+
+	auth := c.Request.Header.Get("Authorization")
+	if auth != "" {
+		tId := strings.TrimPrefix(auth, "Bearer ")
+		t, err := integrations.VerifyIDToken(tId)
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": err.Error()})
+			return
+		}
+		uid = t.UID
+	} else {
+		uid = "Not logged user"
+	}
 
 	if err := c.ShouldBindJSON(&req); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
